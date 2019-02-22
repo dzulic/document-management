617b17d3315f6623a371c0acaa8c69525625ea02
dzulic <julijaciric93@gmail.com>
2019-02-22 23:49:16 +0100

    client menu

--- client-document-management/src/main/frontend/src/app/components/base/HeaderPanel.js
+++ client-document-management/src/main/frontend/src/app/components/base/HeaderPanel.js
@@ -6,13 +6,12 @@ export class HeaderPanel extends Component {
     render() {
         return (
             <div className="row" id="header-panel">
-                <div className="col-lg-12">
-                    <div id="header-panel-logo">
-                        <img src={logo} className="navbar-brand"/>
-                    </div>
-                    <MenuComponent>
-
-                    </MenuComponent>
+                <div className="col-md-2 offset-md-1" id="header-panel-logo">
+                    <img src={logo} className="navbar-brand"/>
+                </div>
+                <div className="col-md-5"></div>
+                <div className="col-md-4">
+                    <MenuComponent/>
                 </div>
             </div>
         );
